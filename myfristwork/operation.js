008bdf9f837c48f88f4fe3f6c46efaf7bf152054
zulqarnainsidd461-rgb <zulqarnainsidd461@gmail.com>
2026-01-22 15:07:21 +0000

    datatype

---
+++ myfristwork/operation.js
@@ -0,0 +1,32 @@
+let value = 3;
+let neg_value = -value;
+// console.log(neg_value);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello";
+// let str2 = "zulqarnian";
+// let str3 = str1 + " " + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+
+// console.log(true);
+// console.log(+true);
+
+// let num1, num2, num3;
+// num1 = num2 = num3 = 2 + 2
+
+let game_counter = 100;
+game_counter++;
+console.log(game_counter);
+
---
+++ myfristwork/score.js
@@ -0,0 +1,22 @@
+// let score = 33;
+// let score = "33gff";
+// let score = null;
+// let score = undefined;
+// let score = true;
+// console.log(typeof(score));
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// let is_logged = 1;
+// let is_logged = "";
+// let is_logged = "zulqarnain";
+// let boolean_is_logged = Boolean(is_logged);
+// console.log(boolean_is_logged);
+
+let some_number = 33;
+let string_number = String(some_number);
+// console.log(typeof(string_number));
+console.log(string_number)
