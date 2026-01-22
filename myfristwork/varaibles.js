b3301ef26311754f43362b5adac1e59bff68b91a
zulqarnainsidd461-rgb <zulqarnainsidd461@gmail.com>
2026-01-22 11:28:03 +0000

    Update variables file

---
+++ myfristwork/varaibles.js
@@ -0,0 +1,7 @@
+const  acount_id = 123456;
+let acount_email = "@zul@gmail.com";
+var acount_password = "12345";
+acount_city = "new york";
+ 
+// acount_id = 12344;
+console.table([acount_id,acount_email,acount_password]);
